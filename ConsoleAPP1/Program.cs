f40c721edb6fc19c77e9528d0ef0613a8f5f16b5
S-Bou <bouyo30@gmail.com>
2023-03-12 19:50:38 +0100

    202303121950

---
+++ ConsoleAPP1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 18;
+
+            bool puertaAbierta = true;
+
+            Console.WriteLine("Vamos a evaluar si eres mayor de edad \n");
+
+            if(edad >= 18 && puertaAbierta) Console.WriteLine("Si eres mayor de edad o la puerta está abierta \n");
+            
+            else Console.WriteLine("No eres mayor de edad o la puerta está cerrada \n");
+
+            Console.WriteLine("Fin del programa");
+        }
+    }
+}
