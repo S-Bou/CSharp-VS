d1099ccf57c2b87ee29f1de4122e56cace279079
S-Bou <bouyo30@gmail.com>
2023-03-15 11:51:49 +0100

    202303151151

--- EjemploPOOCirculo/Program.cs
+++ EjemploPOOCirculo/Program.cs
@@ -7,27 +7,58 @@ namespace EjemploCirculo
     {
         static void Main(string[] args)
         {
+            /*
             Circulo miCirculo;  //Creación de objeto Circulo. Variable/objeto de tipo Círculo
 
             miCirculo = new Circulo();  //Iniciación de variable/objeto. Instanciar una clase. Instanciación
 
-            miCirculo.calculoArea(5);
+            miCirculo.CalculoArea(5);
 
-            Console.WriteLine($"El área es: {miCirculo.calculoArea(5)}");
+            Console.WriteLine($"El área es: {miCirculo.CalculoArea(5)}");
 
             Circulo miCirculo2 = new Circulo();
 
-            Console.WriteLine($"El área del segundo circulo es: {miCirculo2.calculoArea(5)}");
+            Console.WriteLine($"El área del segundo circulo es: {miCirculo2.CalculoArea(5)}");
+            */
+
+            ConversorEuroDolar obj = new ConversorEuroDolar();
+
+            obj.CambiaValorEuro(1.45);
+
+            Console.WriteLine(obj.Convierte(50));
+
         }
     }
 
     class Circulo                       //Clase circulo
     {
-        const double pi = 3.1416;             //Propiedad de la clase. Campo de clase
+        private const double pi = 3.1416;      //Propiedad de la clase. Campo de clase
 
-        public double calculoArea(int radio)   //Método de clase
+        public double CalculoArea(int radio)   //Método de clase
         {
             return pi * radio * radio;
+        }
+    }
+
+    class ConversorEuroDolar
+    {
+        private double euro = 1.253;
+
+        public double Convierte(double cantidad)
+        {
+            return cantidad * euro;
+        }
+
+        public void CambiaValorEuro(double nuevoValor)
+        {
+            if(nuevoValor < 0)
+            { 
+                euro = 1.253; 
+            
+            }else{
+
+                euro = nuevoValor;
+            }
         }
     }
 
