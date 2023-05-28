21fe53a7f22aa6f72cdaa3f7381e9bbbb1e6df4a
S-Bou <bouyo30@gmail.com>
2023-05-28 20:00:22 +0200

    202305281959

---
+++ WpfProperties/JuntaNombre.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+
+public class JuntaNombre : INotifyPropertyChanged
+{
+
+	private string nombre, apellido, nombre_completo;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged(string property)
+    {
+        if (PropertyChanged != null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+    }
+
+    public string Nombre
+    { 
+        get { return nombre; }
+        set 
+        { 
+            nombre = value;
+            OnPropertyChanged("Nombre_Completo"); 
+        }
+    }
+
+    public string Apellido
+    {
+        get { return apellido; }
+        set
+        {
+            apellido = value;
+            OnPropertyChanged("Nombre_Completo");
+        }
+    }
+
+    public string Nombre_Completo
+    {
+        get
+        {
+            nombre_completo = Nombre + " " + Apellido;
+            return nombre_completo;
+        }
+        set { }
+    }
+}
